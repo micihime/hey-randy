9bf5cdfbc5a51a50b26dd7ce6f547c68ce0f51a8
Miroslava Hreskova <hroncovam@yahoo.com>
2024-11-13 17:38:46 +0000

    Formatting

--- app/routes/blog+/how-to-use+/tips+/5.tsx
+++ app/routes/blog+/how-to-use+/tips+/5.tsx
@@ -9,7 +9,7 @@ export default function Index() {
     <main>
       <article>
         <header className="post-header">
-          <h1>Mindful Breathing</h1>
+          <h1>Gratitude Practice</h1>
           {/* <p className="post-meta">
 						written by <a href="https://mitchie.netlify.app/" className="author">Michi</a>
 						<time dateTime="2024-09-20 19:00">Sep. 20, 2024</time>
@@ -17,11 +17,8 @@ export default function Index() {
         </header>
         <div className="post-content">
           <p>
-            How: Sit or lie down comfortably. Close your eyes and bring your
-            attention to your breath. Notice the sensation of the breath as it
-            enters and leaves your nostrils or feel your chest and abdomen
-            rising and falling. If your mind wanders, gently bring it back to
-            your breath. Duration: 5-10 minutes.
+            Remind users to note down something they’re grateful for at random
+            times.
           </p>
           {/* <aside className="pullquote float-left">
 						<q>The paintwater cup has been spilled again. Moment of silence for the carpet.</q>
